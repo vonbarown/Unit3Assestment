d2647606ca8114ce58df59fe133c3b77eb9e3aa4
vonbarown <vonielbrown@pursuit.org>
2019-12-25 18:04:13 -0500

    linking server and react

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import List from './pages/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Are you a cat person or a dog person?</h1>
+        <nav>
+          <Link to="/">Home</Link>{" "}
+          <Link to="/dog">Dog</Link>{" "}
+          <Link to="/cat">Cat</Link>{" "}
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
